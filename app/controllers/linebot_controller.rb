4afb7046c62932ca0c3e4800ebe218b574c00cd2
hiromi fukuda <63894985+hiromi-dev@users.noreply.github.com>
2021-01-15 16:52:53 +0900

    一部編集

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -14,26 +14,56 @@ class LinebotController < ApplicationController
     events.each do |event|
       # eventがユーザーからメッセージが送信された場合
       case event
+      when Line::Bot::Event::Postback
+        LineBot::PostbackEvent.send(event['postback']['data'])
+
       when Line::Bot::Event::Message
-        #テキスト形式の場合
         case event.type
+        #テキスト形式の場合
         when Line::Bot::Event::MessageType::Text
           case event.message['text']
           when /.*(会議|かいぎ).*/
             @meeting_url = ExecutionZoomApi.new.execution_zoom_api
-            content = "ミーティング開始URL : #{@meeting_url['start_url'].slice(0..102)}"
-          when /.*(選択).*/
-          when /.*(かきくけこ).*/
-            content = 'かきくけこ'
-          else
-            content = '違う'
-          end
-          message = {
+            content = "ミーティング開始URLを作成しました。 : #{@meeting_url['start_url'].slice(0..102)}"
+            message = {
               type: 'text',
               text: content
             }
-        client.reply_message(event['replyToken'], message)
-        # テキスト形式以外の場合
+            client.reply_message(event['replyToken'], message)
+          when /.*(選択).*/
+            selectdate = {
+            type: "template",
+            altText: "this is a buttons template",
+            template: {
+              type: "buttons",
+              title: "予約したい日付を指定して下さい。",
+              text: "Please select",
+              actions: [
+                {
+                  type: "datetimepicker",
+                  label: "選択",
+                  mode: "datetime",
+                  data: "action=datetemp&selectId=1"
+                },
+                {
+                  type: "postback",
+                  label: "指定しない",
+                  data: "action=datetemp&selectId=1"
+                },
+              ]
+            }
+          }
+            client.reply_message(event['replyToken'], selectdate)
+          else
+            content = '違う'
+            message = {
+              type: 'text',
+              text: content
+            }
+            client.reply_message(event['replyToken'], message)
+          end
+
+        # スタンプの場合
         when Line::Bot::Event::MessageType::Sticker
           @selectdate = {
             type: "template",
@@ -51,14 +81,14 @@ class LinebotController < ApplicationController
                 },
                 {
                   type: "postback",
-                  label: "やっぱりやめたい",
+                  label: "指定しない",
                   data: "action=cancel&selectId=2"
                 },
               ]
             }
           }
           client.reply_message(event['replyToken'], @selectdate)
-          show_date
+
         end
       end
     end
@@ -66,13 +96,6 @@ class LinebotController < ApplicationController
     head :ok
   end
 
-  def show_date
-    message = {
-              type: 'text',
-              text: "hello"
-            }
-    client.reply_message(event['replyToken'], message)
-  end
 end
 
 class ExecutionZoomApi
